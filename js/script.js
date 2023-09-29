ccc95f573b2f99fbf9d864545946a569bc65b283
FraRussotto <russottofrancesco1@gmail.com>
2023-09-29 14:59:08 +0200

    [edit] start markup

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const { createApp } = Vue;
 createApp({
   data(){
 
-    return
+    return tasks = [
+      {
+        object: 'commissione 1',
+        isDone: false
+      },
+      {
+        object: 'commissione 2',
+        isDone: false
+      },
+      {
+        object: 'commissione 3',
+        isDone: false
+      },
+      {
+        object: 'commissione 4',
+        isDone: false
+      },
+    ]
     
   }
 }).mount('#app')
